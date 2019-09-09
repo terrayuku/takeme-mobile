f44dcfa5fe173ba990a7017d4286f4618720c746
A231049 <simthembile.yuku@standardbank.co.za>
2019-09-10 00:56:45 +0200

    Adding Spinner when loading webview

--- app/src/main/java/com/takeme/takemeto/AboutActivity.java
+++ app/src/main/java/com/takeme/takemeto/AboutActivity.java
@@ -1,13 +1,19 @@
 package com.takeme.takemeto;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class AboutActivity extends AppCompatActivity {
     private WebView webview;
+    ProgressBar simpleProgressBar;
 
 
     @Override
@@ -16,12 +22,35 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.about);
 
         webview =(WebView) findViewById(R.id.webView);
+        simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
 
-        webview.setWebViewClient(new WebViewClient());
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                    simpleProgressBar.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+            @Override
+            public void onPageFinished(final WebView view, String url) {
+
+                simpleProgressBar.setVisibility(View.GONE);
+            }
+        });
+//        webview.setWebViewClient(new MyClient());
+//        client.setFirsttime();
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
         webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+//        webview.getSettings().setUseWideViewPort(true);
+//        webview.getSettings().setLoadWithOverviewMode(true);
         webview.loadUrl("https://terrayuku.github.io/takeme-web/#/about");
+
 
     }
 
@@ -29,5 +58,33 @@ public class AboutActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    public class MyClient extends WebViewClient {
+        private boolean oneTime=false;
+        public void setFirsttime(){
+            oneTime=true;
+        }
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            if(oneTime){
+                simpleProgressBar.setVisibility(View.VISIBLE);
+                oneTime=false;
+            }
+
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(final WebView view, String url) {
+
+            super.onPageFinished(view, url);
+            simpleProgressBar.setVisibility(View.GONE);
+        }
     }
 }
