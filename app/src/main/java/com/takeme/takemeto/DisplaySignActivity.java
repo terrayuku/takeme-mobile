4f5a2724c12318c75802c634611382b79715cdef
MajeedZulqarnain <majeed@innet.co.za>
2019-11-18 10:19:35 +0200

    lowered min sdk

--- app/src/main/java/com/takeme/takemeto/DisplaySignActivity.java
+++ app/src/main/java/com/takeme/takemeto/DisplaySignActivity.java
@@ -1,15 +1,14 @@
 package com.takeme.takemeto;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
